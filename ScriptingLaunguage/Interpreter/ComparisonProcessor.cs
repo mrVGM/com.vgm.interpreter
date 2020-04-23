2794b88b7ac22e7fdd5bece7da9d0cb07d62c6ee
Vasil <magaranov@abv.bg>
2020-04-23 19:40:33 +0300

    refactored comparison processor

--- ScriptingLaunguage/Interpreter/ComparisonProcessor.cs
+++ ScriptingLaunguage/Interpreter/ComparisonProcessor.cs
@@ -29,61 +29,41 @@ namespace ScriptingLaunguage.Interpreter
             ExpressionProcessor.ProcessNode(expr1, scope, ref e1);
             ExpressionProcessor.ProcessNode(expr2, scope, ref e2);
 
+            bool areEqual(object o1, object o2) 
+            {
+                if (o1 == null)
+                {
+                    return o2 == null;
+                }
+
+                if (o1 is float || o1 is int)
+                {
+                    return Convert.ToSingle(o1) == Convert.ToSingle(o2);
+                }
+
+                if (o1 is bool)
+                {
+                    return Convert.ToBoolean(o1) == Convert.ToBoolean(o2);
+                }
+
+                var method = o1.GetType().GetMethod("op_Equality");
+                if (method == null)
+                {
+                    return o1 == o2;
+                }
+
+                return (bool) method.Invoke(null, new object[] { o1, o2 });
+            }
+
             if (programNode.MatchChildren("Expression", "==", "Expression")) 
             {
-                if (e1 == null) 
-                {
-                    value = e2 == null;
-                    return null;
-                }
-
-                if (e1 is float || e1 is int) 
-                {
-                    value = Convert.ToSingle(e1) == Convert.ToSingle(e2);
-                    return true;
-                }
-
-                if (e1 is bool) {
-                    value = Convert.ToBoolean(e1) == Convert.ToBoolean(e2);
-                    return null;
-                }
-
-                var method = e1.GetType().GetMethod("op_Equality");
-                if (method == null) {
-                    value = e1 == e2;
-                    return null;
-                }
-
-                value = method.Invoke(null, new object[] { e1, e2 });
+                value = areEqual(e1, e2);
                 return true;
             }
 
             if (programNode.MatchChildren("Expression", "!=", "Expression"))
             {
-                if (e1 == null)
-                {
-                    value = e2 != null;
-                    return null;
-                }
-
-                if (e1 is float || e1 is int)
-                {
-                    value = Convert.ToSingle(e1) != Convert.ToSingle(e2);
-                    return true;
-                }
-                
-                if (e1 is bool) {
-                    value = Convert.ToBoolean(e1) != Convert.ToBoolean(e2);
-                    return null;
-                }
-
-                var method = e1.GetType().GetMethod("op_Inequality");
-                if (method == null) {
-                    value = e1 != e2;
-                    return null;
-                }
-
-                value = method.Invoke(null, new object[] { e1, e2 });
+                value = !areEqual(e1, e2);
                 return true;
             }
 
