3aee9247e61f56ea8deaa17a0c070b4ea1095dd0
Vasil <magaranov@abv.bg>
2020-05-06 18:36:38 +0300

    extended exception logging

--- ScriptingLanguage/Interpreter/IProgramNodeProcessor.cs
+++ ScriptingLanguage/Interpreter/IProgramNodeProcessor.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using ScriptingLaunguage.Parser;
 using ScriptingLaunguage.Tokenizer;
 using static ScriptingLaunguage.Utils;
@@ -7,31 +10,59 @@ namespace ScriptingLaunguage.Interpreter
 {
     public class BasicLanguageException : LanguageException
     {
-        public BasicLanguageException(string message, ProgramNode exceptionNode) : base(message, exceptionNode.GetScriptSource(), exceptionNode.GetCodeIndex())
+        private string callStack = "";
+        public BasicLanguageException(string message, ProgramNode exceptionNode, Stack<ProgramNode> programStack) : base(message, exceptionNode.GetScriptSource(), exceptionNode.GetCodeIndex())
         {
+            int lastLine = -1;
+            ScriptId lastScript = null;
+            foreach (var node in programStack) 
+            {
+                var scriptSource = node.GetScriptSource();
+                if (string.IsNullOrEmpty(scriptSource.Filename))
+                {
+                    break;
+                }
+
+                string script = scriptSource.Script;
+                int line = Utils.GetLineNumber(CodeIndex, script);
+                if (line == lastLine && scriptSource == lastScript) 
+                {
+                    continue;
+                }
+
+                int index = Utils.GetLineOffset(CodeIndex, script);
+                lastLine = line;
+                lastScript = scriptSource;
+
+                callStack += $"{scriptSource.Filename} {line + 1}:{index + 1}{Environment.NewLine}";
+            }
         }
 
         public override string GetErrorMessage(bool printLineNumbers)
         {
-            return GetCodeSample(CodeIndex, ScriptId.Script, true);
+            return $"{GetCodeSample(CodeIndex, ScriptId.Script, true)}{Environment.NewLine}{callStack}";
         }
     }
     public class NodeProcessor
     {
-        private static NodeProcessor Instance = new NodeProcessor();
+        private static Stack<ProgramNode> programStack = new Stack<ProgramNode>();
         
         public static object ExecuteProgramNodeProcessor(IProgramNodeProcessor processor, ProgramNode programNode, Scope scope, ref object value)
         {
             try
             {
-                return processor.ProcessNode(programNode, scope, ref value);
+                programStack.Push(programNode);
+                var res = processor.ProcessNode(programNode, scope, ref value);
+                programStack.Pop();
+                return res;
             }
             catch (Exception e) 
             {
                 LanguageException languageException = e as LanguageException;
                 if (languageException == null) {
-                    languageException = new BasicLanguageException(e.Message, programNode);
+                    languageException = new BasicLanguageException($"{e.Message} {e.InnerException?.Message}", programNode, programStack);
                 }
+                programStack.Clear();
                 throw languageException;
             }
         }
