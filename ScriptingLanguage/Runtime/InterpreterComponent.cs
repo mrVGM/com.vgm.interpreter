e0ccd405fda6f192ce7f6772fe0f23c191a56eb1
Vasil <magaranov@abv.bg>
2020-06-25 23:49:16 +0300

    unity package version

---
+++ ScriptingLanguage/Runtime/InterpreterComponent.cs
@@ -0,0 +1,17 @@
+﻿using ScriptingLaunguage.Interpreter;
+using UnityEngine;
+
+namespace ScriptingLaunguage
+{
+    public class InterpreterComponent : MonoBehaviour
+    {
+        public string ScriptsFolder = "RuntimeScripts/";
+        [TextArea(30, 50)]
+        public string Output;
+        public string Command;
+        
+        public Session Session;
+        public Interpreter.Interpreter Interpreter;
+
+    }
+}
