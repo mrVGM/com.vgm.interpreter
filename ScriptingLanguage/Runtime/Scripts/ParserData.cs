ba4f0ea91a68e5df396c48009dca7f87aa067ac5
Vasil <magaranov@abv.bg>
2020-10-10 20:22:50 +0300

    added interpreter session holder script

---
+++ ScriptingLanguage/Runtime/Scripts/ParserData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace ScriptingLanguage
+{
+    public class ParserData : ScriptableObject
+    {
+        public TextAsset ParserTable;
+    }
+}
---
+++ ScriptingLanguage/Runtime/Scripts/SessionHolder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace ScriptingLanguage
+{
+    public class SessionHolder : MonoBehaviour
+    {
+        [SerializeField]
+        [HideInInspector]
+        private TextAsset ParserTable;
+
+        REPL.REPL repl;
+        REPL.REPL REPL
+        {
+            get
+            {
+                if (repl == null) {
+                    var parserData = Resources.FindObjectsOfTypeAll<ParserData>().FirstOrDefault();
+                    var pt = Parser.ParserTable.Deserialize(parserData.ParserTable.bytes);
+                    repl = new REPL.REPL(pt);
+                }
+                return repl;
+            }
+        }
+
+        public IEnumerable<string> RunCommand(string command)
+        {
+            if (string.IsNullOrEmpty(command)) {
+                yield return command;
+            }
+
+            var res = REPL.HandleCommand(command);
+            foreach (var str in res)
+            {
+                yield return str;
+            }
+        }
+    }
+}
