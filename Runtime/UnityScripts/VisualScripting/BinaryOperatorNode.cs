81a990048992b5741660bd106322dc7bec472823
Vasil <magaranov@abv.bg>
2020-12-14 01:34:44 +0200

    function call node

---
+++ Runtime/UnityScripts/VisualScripting/BinaryOperatorNode.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ScriptingLanguage.VisualScripting
+{
+    public class BinaryOperatorNode : MonoBehaviour, ICodeGenerator, IKnobOwner
+    {
+        public Text Name;
+        public InputField InputField;
+        public Knob LeftKnob;
+        public Knob RightKnob;
+        public Knob OutputKnob;
+
+        public void EnableTitleInputField()
+        {
+            if (InputField.gameObject.activeSelf) {
+                return;
+            }
+
+            InputField.gameObject.SetActive(true);
+            InputField.text = Name.text;
+            InputField.ActivateInputField();
+        }
+
+        public void OnTitleChanged()
+        {
+            if (string.IsNullOrWhiteSpace(InputField.text)) {
+                return;
+            }
+
+            if (InputField.text.Contains(" ")) {
+                return;
+            }
+
+            Name.text = InputField.text;
+            InputField.gameObject.SetActive(false);
+        }
+
+        public string GenerateCode(Knob knob)
+        {
+            if (knob != OutputKnob) {
+                throw new NotSupportedException();
+            }
+
+            var leftEndpoint = LeftKnob.LinkEndpoints.FirstOrDefault();
+            var rightEndpoint = RightKnob.LinkEndpoints.FirstOrDefault();
+            
+            var leftLinkedKnob = leftEndpoint.GetCounterpart().Knob;
+            var rightLinkedKnob = rightEndpoint.GetCounterpart().Knob;
+            var leftCodeGenerator = leftLinkedKnob.GetComponentInParent<ICodeGenerator>();
+            var rightCodeGenerator = rightLinkedKnob.GetComponentInParent<ICodeGenerator>();
+
+            return $"({leftCodeGenerator.GenerateCode(leftLinkedKnob)}{Name}{rightCodeGenerator.GenerateCode(rightLinkedKnob)})";
+        }
+
+        public IEnumerator<Knob> GetEnumerator()
+        {
+            yield return LeftKnob;
+            yield return RightKnob;
+            yield return OutputKnob;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Runtime/UnityScripts/VisualScripting/FunctionCallNode.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace ScriptingLanguage.VisualScripting
+{
+    public class FunctionCallNode : MonoBehaviour, ICodeGenerator, IKnobOwner
+    {
+        enum KnobOperation
+        {
+            Add,
+            Remove,
+        }
+        
+        public Knob KnobPrefab;
+        public RectTransform TemplateArgs;
+        public RectTransform Args;
+        public Knob Function;
+        public Knob Output;
+
+        private void UpdateKnobs(KnobOperation knobOperation, RectTransform parentTransform)
+        {
+            if (knobOperation == KnobOperation.Add) {
+                Instantiate(KnobPrefab, parentTransform);
+                return;
+            }
+
+            if (knobOperation == KnobOperation.Remove) {
+                var knob = parentTransform.GetComponentsInChildren<Knob>().FirstOrDefault();
+                foreach (var linkEndpoint in KnobPrefab.LinkEndpoints) {
+                    linkEndpoint.GetLink().DestroyLink();
+                }
+
+                Destroy(knob.gameObject);
+            }
+        }
+
+        public void AddTemplateArg()
+        {
+            UpdateKnobs(KnobOperation.Add, TemplateArgs);
+        }
+        
+        public void RemoveTemplateArg()
+        {
+            UpdateKnobs(KnobOperation.Remove, TemplateArgs);
+        }
+        
+        public void AddArg()
+        {
+            UpdateKnobs(KnobOperation.Add, Args);
+        }
+        
+        public void RemoveArg()
+        {
+            UpdateKnobs(KnobOperation.Remove, Args);
+        }
+
+        public void RunFunction()
+        {
+            var frame = GetComponentInParent<Frame>();
+            string code = $"{GenerateCode(Output)};";
+            frame.SessionHolder.RunCommand(code).ToList();
+        }
+
+        public string GenerateCode(Knob knob)
+        {
+            var funcKnob = Function.LinkEndpoints.FirstOrDefault().GetCounterpart().Knob;
+            var funcCodeGenerator = funcKnob.GetComponentInParent<ICodeGenerator>();
+
+            var templateArgKnobs = TemplateArgs.GetComponentsInChildren<Knob>();
+            
+            var argKnobs = Args.GetComponentsInChildren<Knob>();
+
+            string templateArgs = "";
+            if (templateArgKnobs.Any()) {
+                templateArgs += "|";
+                foreach (var templateArgKnob in templateArgKnobs) {
+                    var counterPart = templateArgKnob.LinkEndpoints.FirstOrDefault().GetCounterpart().Knob;
+                    var codeGenerator = counterPart.GetComponentInParent<ICodeGenerator>();
+                    templateArgs += $"{codeGenerator.GenerateCode(counterPart)}|";
+                }
+            }
+            
+            string args = "";
+            foreach (var argKnob in argKnobs) {
+                var counterPart = argKnob.LinkEndpoints.FirstOrDefault().GetCounterpart().Knob;
+                var codeGenerator = counterPart.GetComponentInParent<ICodeGenerator>();
+                args += $"{codeGenerator.GenerateCode(counterPart)},";
+            }
+            if (args.Length > 1) {
+                args = args.Substring(0, args.Length - 1);
+            }
+            args = $"({args})";
+            
+            return $"{funcCodeGenerator.GenerateCode(funcKnob)}{templateArgs}{args}";
+        }
+
+        public IEnumerator<Knob> GetEnumerator()
+        {
+            yield return Function;
+            foreach (var knob in TemplateArgs.GetComponentsInChildren<Knob>()) {
+                yield return knob;
+            }
+            foreach (var knob in Args.GetComponentsInChildren<Knob>()) {
+                yield return knob;
+            }
+            yield return Output;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
